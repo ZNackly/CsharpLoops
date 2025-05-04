fba8b939e320159b90f532cf5974773b0e853f62
Dan <137257813+ZNackly@users.noreply.github.com>
2025-05-04 11:34:54 +0500

    Create Program1.cs

---
+++ Program1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int col = 0;
+            int sum = 0;
+            string ok = null;
+            while (sum < 100)
+            {
+                sum += int.Parse(Console.ReadLine());
+                col++;
+            }
+            if (col % 10 == 2 || col % 10 == 3 || col % 10 == 4)
+            {
+                ok = "ла";
+            }else if(col % 10 == 1)
+            {
+                ok = "ло";
+            }
+            else
+            {
+                ok = "eл";
+            }
+                Console.WriteLine($"Было введено {col} чис{ok}. Сумма чисел {sum}");
+        }
+    }
+}
